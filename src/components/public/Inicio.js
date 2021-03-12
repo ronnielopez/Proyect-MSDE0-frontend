593755c6dc11804aa742d6191c5e74cf4caf37b5
ronnielopez <ronnielopez503@gmail.com>
2021-03-12 14:42:19 -0600

    Inicio terminado

--- src/components/public/Inicio.js
+++ src/components/public/Inicio.js
@@ -1,5 +1,8 @@
 import React, { useState } from 'react';
 import {Carousel} from 'react-bootstrap';
+import Img1 from '../../img/gallery/about1.png';
+import Img2 from '../../img/gallery/about2.png';
+import {Link} from 'react-router-dom';
 
 
 function Inicio() {
@@ -23,8 +26,7 @@ function Inicio() {
           alt="First slide"
         />
         <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+       
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
@@ -35,8 +37,7 @@ function Inicio() {
         />
 
         <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+       
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
@@ -47,15 +48,218 @@ function Inicio() {
         />
 
         <Carousel.Caption>
-          <h3>Third slide label</h3>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
         </Carousel.Caption>
       </Carousel.Item>
     </Carousel>
-
     
+    <div className="about-area section-padding2">
+        <div className="container">
+            <div className="row">
+                <div className="col-lg-6 col-md-10">
+                    <div className="about-caption mb-50">
+                        
+                        <div className="section-tittle section-tittle2 mb-35">
+                            <span>Acerca de nosotros</span>
+                            <h2>Bienvenido a Alphamedic</h2>
+                        </div>
+                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quia doloremque repellendus 
+                          placeat accusamus accusantium earum quisquam, totam reprehenderit officiis! Ipsam consequatur
+                           possimus incidunt voluptatem, rem illo esse aliquid cum laborum.</p>
+                           <div className="mb-30 container">
+                              <div className="my-1">
+                                <Link className="btn btn-outline-success w-50" to="/Clinicas">Mira nuestras clinicas</Link>  
+                              </div>
+                              <div className="my-5">
+                                <Link className="btn btn-outline-success w-50" to="/Servicios">Observa todos los beneficios</Link>  
+                              </div>
+                              <div className="my-1¿5">
+                                <Link className="btn btn-outline-success w-50" to="/Registrar">Empieza Ya !!</Link>  
+                              </div>
+                           </div>
+                    </div>
+                </div>
+                <div className="col-lg-6 col-md-12">
+                    
+                    <div className="about-img ">
+                        <div className="about-font-img d-none d-lg-block">
+                            <img src={Img2} alt=""/>
+                        </div>
+                        <div className="about-back-img ">
+                            <img src={Img1} alt=""/>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div className="department_area section-padding2">
+        <div className="container">
+            <div className="row">
+                <div className="col-lg-12">
+                    <div className="section-tittle text-center mb-100">
+                        <span>Servicios</span>
+                        <h2>Nuestros servicios</h2>
+                    </div>
+                </div>
+            </div>
+          </div>
+  <div className="all-starups-area testimonial-area fix">
+        <div className="starups">
+           
+            <div className="h1-testimonial-active">
+                
+                <div className="single-testimonial text-center">
+                    
+                    <div className="testimonial-caption ">
+                      <div className="section-tittle text-center">
+                      <h2>Siempre cuidando de ti</h2>
+                      </div>   
+                        <div className="testimonial-top-cap">
+                            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quia doloremque repellendus 
+                          placeat accusamus accusantium earum quisquam, totam reprehenderit officiis! Ipsam consequatur
+                           possimus incidunt voluptatem, rem illo esse aliquid cum laborum.</p>
+                        </div>
+                        <Link to="/Servicios" className="btn btn-warning w-100 letra">Ver mas servicios</Link>
+                    </div>
+                </div>
+            </div>
+       
+        </div>
+        
+        <div className="starups-img"></div>
+    </div>
+</div>
+
+    <div className="container">
+            <div className="row">
+                <div className="col-lg-12">
+                    <div className="section-tittle text-center mb-100">
+                        <span>Empieza ya</span>
+                        <h2>Nuestros tarifas</h2>
+                    </div>
+                </div>
+            </div>
+          </div>
+    
+          <section className="pricing-plans text-center">
+		<div className="container">
+			<div className="row">
+				<div className="col-md-4 animated slideInRight">
+					<div className="card pricing-box rounded">
+						<div className="card-block">
+							<h4 className="card-title">
+								Basic
+							</h4>
+							<h6 className="card-text">
+								<sup className="currency">
+									$
+								</sup>
+								<span className="amount">
+									9
+								</span>
+								<span className="month">
+									/ mo
+								</span>
+							</h6>
+						</div>
+						<ul className="list-group list-group-flush">
+							<li className="list-group-item text-center d-inline-block">
+								Lorem
+							</li>
+							<li className="list-group-item text-center d-inline-block">
+								Ipsum
+							</li>
+							<li className="list-group-item text-center d-inline-block">
+								datare
+							</li>
+							<li className="list-group-item text-center d-inline-block">
+								Toye
+							</li>
+						</ul>
+						<div className="card-block">
+            <Link to="/Register" className="btn btn-outline-start">Empieza Ya</Link>
+						</div>
+					</div>
+				</div>
+				<div className="col-md-4">
+					<div className="card pricing-box pricing-premium">
+						<div className="card-block">
+							<h4 className="card-title">
+								Premium
+							</h4>
+							<h6 className="card-text">
+								<sup className="currency">
+									$
+								</sup>
+								<span className="amount">
+									19
+								</span>
+								<span className="month">
+									/ mo
+								</span>
+							</h6>
+						</div>
+						<ul className="list-group list-group-flush">
+            <li className="list-group-item text-center d-inline-block">
+								Lorem
+							</li>
+							<li className="list-group-item text-center d-inline-block">
+								Ipsum
+							</li>
+							<li className="list-group-item text-center d-inline-block">
+								datare
+							</li>
+							<li className="list-group-item text-center d-inline-block">
+								Toye
+							</li>
+						</ul>
+						<div className="card-block">
+            <Link to="/Register" className="btn btn-start">Empieza Ya</Link>
+						</div>
+					</div>
+				</div>
+				<div className="col-md-4 animated slideInLeft">
+					<div className="card pricing-box">
+						<div className="card-block">
+							<h4 className="card-title">
+								Gold
+							</h4>
+							<h6 className="card-text">
+								<sup className="currency">
+									$
+								</sup>
+								<span className="amount">
+									39
+								</span>
+								<span className="month">
+									/ mo
+								</span>
+							</h6>
+						</div>
+						<ul className="list-group list-group-flush">
+            <li className="list-group-item text-center d-inline-block">
+								Lorem
+							</li>
+							<li className="list-group-item text-center d-inline-block">
+								Ipsum
+							</li>
+							<li className="list-group-item text-center d-inline-block">
+								datare
+							</li>
+							<li className="list-group-item text-center d-inline-block">
+								Toye
+							</li>
+						</ul>
+						<div className="card-block">
+						<Link to="/Register" className="btn btn-outline-start">Empieza Ya</Link>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
     </>
   );
 }
--- src/layout.jsx
+++ src/layout.jsx
@@ -2,8 +2,9 @@ import React from 'react';
 import { Route, Switch, Redirect, Link } from 'react-router-dom';
 import SiteRoutes from './routes/routing.jsx';
 import Logo from './img/logo/logo.png';
+import Logo2 from './img/logo/logo2_footer.png';
 import Loder from './img/logo/loder.png';
-import {Navbar, Nav} from 'react-bootstrap'
+import { Navbar, Nav } from 'react-bootstrap'
 import './css/style.css';
 import './js/main.js';
 
@@ -35,17 +36,17 @@ export default class Fulllayout extends React.Component {
                             <Link to="/Inicio" className='text-dark nav-link'>Inicio</Link>
                             <Link to="/Nosotros" className='text-dark nav-link'>Nosotros</Link>
                             <Link to="/Servicios" className='text-dark nav-link'>Servicios</Link>
-                            <Link to="/Contactos" className='text-dark nav-link'>Contactenos</Link>
+                            <Link to="/Clinicas" className='text-dark nav-link'>Clinicas</Link>
                         </Nav>
                         <Nav>
-                        <Link to="/Login" className='text-secondary nav-link' >Registrar</Link>
-                        <Link to="/Login" className='text-secondary nav-link' >Iniciar Sesion</Link>
-                      
+                            <Link to="/Login" className='text-secondary nav-link' >Registrar</Link>
+                            <Link to="/Login" className='text-secondary nav-link' >Iniciar Sesion</Link>
+
                         </Nav>
                     </Navbar.Collapse>
                 </Navbar>
 
-               
+
                 <Switch>
                     {SiteRoutes.map((prop, key) => {
                         if (prop.redirect) {
@@ -57,7 +58,67 @@ export default class Fulllayout extends React.Component {
                         }
                     })}
                 </Switch>
-                
+
+
+                <footer>
+                    <div className="footer-area section-bg">
+                        <div className="container">
+                            <div className="footer-top footer-padding">
+                                <div className="row d-flex justify-content-between">
+                                    <div className="col-xl-3 col-lg-3 col-md-4 col-sm-8">
+                                        <div className="single-footer-caption mb-50">
+                                            
+                                <div className="footer-logo">
+                                    <a href="index.html"><img src={Logo2} alt=""/></a>
+                                </div>
+                                            </div>
+                                        </div>
+                                        <div className="col-xl-3 col-lg-4 col-md-6 col-sm-5">
+                                            <div className="single-footer-caption mb-50">
+                                                <div className="footer-tittle">
+                                                    <h4>Acerca de nosotros</h4>
+                                                    <div className="footer-pera">
+                                                        <p className="info1">Lorem igpsum doldfor sit amet, adipiscing elit, sed do eiusmod tempor cergelit rgh. </p>
+                                                        <p className="info1">Lorem ipsum dolor sit amet, adipiscing elit.</p>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div className="col-xl-4 col-lg-4 col-md-6 col-sm-8">
+                                            <div className="single-footer-caption mb-50">
+                                                <div className="footer-number mb-50">
+                                                    <h4><span>+503 </span>2257 7777</h4>
+                                                    <p>Alphamedic@gmail.com</p>
+                                                </div>
+                                           
+                                                    <div className="footer-form">
+                                                    
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="footer-bottom">
+                                        <div className="row d-flex justify-content-between align-items-center">
+                                            <div className="col-xl-9 col-lg-8">
+                                                <div className="footer-copy-right">
+                                                    
+                                                </div>
+                                            </div>
+                                            <div className="col-xl-3 col-lg-4">
+                                                <div className="footer-social f-right">
+                                                    <a href="#"><i className="fab fa-twitter"></i></a>
+                                                    <a href="https://www.facebook.com/sai4ull"><i className="fab fa-facebook-f"></i></a>
+                                                    <a href="#"><i className="fas fa-globe"></i></a>
+                                                    <a href="#"><i className="fab fa-instagram"></i></a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            
+    </footer>
             </>
         )
     }
