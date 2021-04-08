65ded2d04e1686b8e85f81514064f80285f380bb
MacroLions <00175617@uca.edu.sv>
2021-04-07 22:52:17 -0600

    Tarjeta manda a perfil

--- src/components/public/Tarjeta.js
+++ src/components/public/Tarjeta.js
@@ -3,14 +3,16 @@ import { Jumbotron, Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import Cards from 'react-credit-cards'
 import 'react-credit-cards/es/styles-compiled.css'
+import swal from 'sweetalert';
 
 
 function Tarjeta() {
+  const nombreString = new String(sessionStorage.getItem('Nombre'));
   const [data, setData] = useState({
     cvc: "",
     expiry: "08/23",
     focus: "",
-    name:"Tu nombre",
+    name: nombreString,
     number: ""
   });
   
@@ -19,6 +21,16 @@ function Tarjeta() {
       ...data,
       [e.target.name]: e.target.value
     });
+  };
+
+  const procesarTarjeta = () => {
+    if(useState.cvc===undefined&&useState.expiry===undefined&&useState.name===undefined&&useState.number===undefined){
+      console.log("B");
+    }else{
+      console.log("A");
+      swal({ title: 'Tarjeta aceptada!', icon: 'success', button: 'Aceptar', closeOnClickOutside: false, closeOnEsc: false }).then(value => { window.location.href = '#/Perfil'; });
+    }
+    
   };
   
 
@@ -44,12 +56,14 @@ function Tarjeta() {
             name="number"
             placeholder="Numeros Tarjeta"
             onChange={handleInputChange}
+            required
           />
           <input
             type="text"
             name="name"
-            placeholder="Tu nombre"
+            placeholder={sessionStorage.getItem('Nombre')}
             onChange={handleInputChange}
+            required
           />
           <input
             type="tel"
@@ -57,6 +71,7 @@ function Tarjeta() {
             placeholder="08/23"
             onChange={handleInputChange}
             maxlength= "9"
+            required
           />
           <input
             type="number"
@@ -64,12 +79,14 @@ function Tarjeta() {
             placeholder="CVC"
             onChange={handleInputChange}
             maxlength= "4"
+            required
           />
-
-        <button class="btn btn-primary" type="submit">Guardar tarjeta</button>  
+        <div class="vertical-center">
+        <button  class="btn btn-primary" type="submit" onClick={procesarTarjeta} >Guardar tarjeta</button> 
+        </div> 
         </form>
       </div>
-    
+      
       <div className="container">
         
       </div>
